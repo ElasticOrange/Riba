7c3fcd14a194661f3c7ffffe743712e185f6c985
Mihai Barbu <xwave21@gmail.com>
2014-11-28 18:12:25 +0200

    Add fully working riba prototype

--- riba-prototype.sh
+++ riba-prototype.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-foldername=$(date +%Y-%m-%d_%H-%M-%S)
-mkdir -p  /home/mikes/Desktop/Backups/ElasticOrange/"$foldername"/site/	#archive and split in 240MB parts, create folder timestamp
+sudo mount /home/mikes/box.com
 
-tar -cpz /usr/share/nginx/unique-romania.studio/ | split -d -b 240m - /home/mikes/Desktop/Backups/ElasticOrange/"$foldername"/site/site_$(date +%Y-%m-%d_%H-%M-%S).tar.gz
+foldername=$(date +%Y-%m-%d_%H-%M-%S)
+
+sudo tar -cpz /usr/share/nginx/unique-romania.studio/ | split -d -b 240m - /usr/share/nginx/site_$(date +%Y-%m-%d_%H-%M-%S).tar.gz
+
+sudo mkdir -p  /home/mikes/box.com/Backups/ElasticOrange/"$foldername"/site/
+
+rsync -azvv /usr/share/nginx/site_* /home/mikes/box.com/Backups/ElasticOrange/"$foldername"/site/
+
+sudo rm -rf /usr/share/nginx/site_* 
+
+#la mine nu merg comenzile fara sudo
+#pe server ne logam de obicei cu root deci sudo nu va mai fi necesar
