208f2b421df9389e3b4dc560c9e94bbebc48b505
Mihai Barbu <xwave21@gmail.com>
2014-11-28 16:40:48 +0200

    Add archive command with split for every 240mb

--- riba-prototype.sh
+++ riba-prototype.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+#archive and split in 240MB parts
+tar -cpz /usr/share/nginx/unique-romania.studio/ | split -d -b 240m - /home/mikes/Desktop/site_$(date "+%Y-%d-%b_%H-%M-%S").tar.gz
+
+$(date "+%Y-%d-%b_%H-%M-%S")
+$ DATE = 'date +%y-%m-%d_%H-%M-%S'
+$ FILE=site.tar.gz
+$ FILE=${FILE%.*}_'date +%y-%m-%d_%H-%M-%S'.${FILE#*.}
+$ echo $FILE
+
+
+mkdir -p to create missing directory
